084104b5137a2c3f22dcfc586a1eaaf9c6eafdb3
HuyenPham <huyen.phamhoang96@gmail.com>
2023-02-15 14:25:54 +0700

    testing

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
-// app.get("/", (_req: Request, res: Response) => {
-//   return res.send("Express Typescript on Vercel");
-// });
-// app.get("/ping", (_req: Request, res: Response) => {
-//   return res.send("pong 🏓");
-// });
+app.use("/", (_req, res) => {
+    return res.send("Express Typescript on Vercel");
+});
+app.get("/ping", (_req, res) => {
+    return res.send("pong 🏓");
+});
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
